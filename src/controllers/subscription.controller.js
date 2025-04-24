4bac0a36e2b17daec007b65debc08250f2c7e843
Subhajit Dash <dashsubhajit702@gmail.com>
2025-04-24 14:12:17 +0530

    feat: Add user channel subscriber retrieval and subscribed channel fetching endpoint

--- src/controllers/subscription.controller.js
+++ src/controllers/subscription.controller.js
@@ -37,4 +37,71 @@ const toggleSubscription = asyncHandler(async (req, res) => {
     .json(new apiResponse(200, {}, "Subscribed Successfully"));
 });
 
-export { toggleSubscription };
+const getUserChannelSubscribers = asyncHandler(async (req, res) => {
+  const { channelId } = req.params;
+
+  if (!channelId) {
+    throw new apiError(400, "Channel id is required");
+  }
+
+  const totalSubscriber = await Subscription.countDocuments({
+    channel: channelId,
+  }); // return only numbers
+
+  if (!totalSubscriber) {
+    throw new apiError(404, "Not found any subscriber for this channel");
+  }
+
+  // const subscribers = await Subscription.find(
+  //   {channel: channelId}
+  // ).populate("subscriber", "fullname email", )
+
+  // if (!subscribers.length) {
+  //   throw new apiError(404, "Not found any subscriber for this channel")
+  // }
+
+  return res.status(200).json(
+    new apiResponse(
+      200,
+      {
+        // totalSubscriber: subscribers.length,
+        totalSubscriber,
+      },
+      "Subscriber count fetched Successfully"
+    )
+  );
+});
+
+const getSubscribedChannel = asyncHandler(async (req, res) => {
+  const subscriberId = req.user._id;
+
+  if (!subscriberId) {
+    throw new apiError(400, "Subscriber id is required");
+  }
+
+  const channel = await Subscription.find({
+    subscriber: subscriberId,
+  }).populate("channel", "fullname username");
+
+  if (!channel.length) {
+    throw new apiError(404, "Not found any subscribed channel");
+  }
+
+  return res.status(200).json(
+    new apiResponse(
+      200,
+      {
+        totalSubscribedChannel: channel.length,
+        subscribedChannel: channel,
+      },
+      "Subscribed channel fetched Successfully"
+    )
+  );
+});
+
+export { 
+  toggleSubscription, 
+  getUserChannelSubscribers, 
+  getSubscribedChannel,
+
+};
--- src/routes/subscription.routes.js
+++ src/routes/subscription.routes.js
@@ -1,12 +1,19 @@
 import { Router } from "express";
-import {verifyJWT} from '../middlewares/auth.middleware.js'
-import {toggleSubscription} from '../controllers/subscription.controller.js'
+import { verifyJWT } from "../middlewares/auth.middleware.js";
+import {
+  getSubscribedChannel,
+  getUserChannelSubscribers,
+  toggleSubscription,
+} from "../controllers/subscription.controller.js";
 
-const router = Router()
-router.use(verifyJWT)
+const router = Router();
+router.use(verifyJWT);
 
-router.route("/channel/:channelId")
-.post(toggleSubscription)
+router
+  .route("/channel/:channelId")
+  .post(toggleSubscription)
+  .get(getUserChannelSubscribers);
 
+router.route("/channel").get(getSubscribedChannel);
 
-export default router
+export default router;
