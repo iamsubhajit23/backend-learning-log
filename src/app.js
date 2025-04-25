d9e209721d83c4b2279ce8eca321126e7362e310
Subhajit Dash <dashsubhajit702@gmail.com>
2025-04-26 03:39:28 +0530

    feat: Add playlist router with CRUD operations for managing playlists

--- src/app.js
+++ src/app.js
@@ -30,11 +30,13 @@ import videoRouter from "./routes/video.routes.js";
 import subscriptionRouter from "./routes/subscription.routes.js";
 import likeRouter from "./routes/like.routes.js";
 import commentRouter from "./routes/comment.routes.js";
+import playlistRouter from "./routes/playlist.router.js"
 
 app.use("/api/v1/user/", userRouter );
 app.use("/api/v1/video/", videoRouter );
 app.use("/api/v1/subscription/", subscriptionRouter );
 app.use("/api/v1/like/", likeRouter );
 app.use("/api/v1/comment/", commentRouter );
+app.use("/api/v1/playlist/", playlistRouter );
 
 export { app };
---
+++ src/routes/playlist.router.js
@@ -0,0 +1,37 @@
+import { Router } from "express";
+import { verifyJWT } from "../middlewares/auth.middleware.js";
+import {
+  addVideoToPlaylist,
+  createPlaylist,
+  deletePlaylist,
+  getPlaylistById,
+  getUserPlaylists,
+  removeVideoFromPlaylist,
+  togglePublishStatus,
+  updatePlaylistInfo,
+} from "../controllers/playlist.controller.js";
+
+const router = Router();
+router.use(verifyJWT);
+
+
+router.route("/create").post(createPlaylist);
+
+router.route("/add-video/:playlistId/:videoId").patch(addVideoToPlaylist);
+
+router
+  .route("/remove-video/:playlistId/:videoId")
+  .patch(removeVideoFromPlaylist);
+
+router.route("/update-playlist/:playlistId").patch(updatePlaylistInfo);
+
+router.route("/:playlistId").get(getPlaylistById);
+
+router.route("/").get(getUserPlaylists);
+
+router.route("/toggle-publish-status/:playlistId").patch(togglePublishStatus);
+
+router.route("/delete-playlist/:playlistId").delete(deletePlaylist);
+
+
+export default router;
