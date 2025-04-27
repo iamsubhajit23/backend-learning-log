779d85e7d498558b0c57aaaaea5edbc0a7258182
Subhajit Dash <dashsubhajit702@gmail.com>
2025-04-28 00:29:16 +0530

    feat: Add dashboard route to app for handling dashboard-related requests

--- src/app.js
+++ src/app.js
@@ -32,6 +32,7 @@ import likeRouter from "./routes/like.routes.js";
 import commentRouter from "./routes/comment.routes.js";
 import playlistRouter from "./routes/playlist.router.js";
 import tweetRouter from "./routes/tweet.routes.js";
+import dashboardRouter from "./routes/dashboard.routes.js";
 
 app.use("/api/v1/user/", userRouter );
 app.use("/api/v1/video/", videoRouter );
@@ -40,5 +41,7 @@ app.use("/api/v1/like/", likeRouter );
 app.use("/api/v1/comment/", commentRouter );
 app.use("/api/v1/playlist/", playlistRouter );
 app.use("/api/v1/tweet/", tweetRouter );
+app.use("/api/v1/dashboard/", dashboardRouter );
+
 
 export { app };
