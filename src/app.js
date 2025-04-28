2fa1de2e1075c222c6efd71f1a7002d9cb0839f1
Subhajit Dash <dashsubhajit702@gmail.com>
2025-04-28 13:08:57 +0530

    feat: Add healthcheck route to app for monitoring system health

--- src/app.js
+++ src/app.js
@@ -33,6 +33,7 @@ import commentRouter from "./routes/comment.routes.js";
 import playlistRouter from "./routes/playlist.router.js";
 import tweetRouter from "./routes/tweet.routes.js";
 import dashboardRouter from "./routes/dashboard.routes.js";
+import healthcheckRouter from "./routes/healthcheck.routes.js"
 
 app.use("/api/v1/user/", userRouter );
 app.use("/api/v1/video/", videoRouter );
@@ -42,6 +43,6 @@ app.use("/api/v1/comment/", commentRouter );
 app.use("/api/v1/playlist/", playlistRouter );
 app.use("/api/v1/tweet/", tweetRouter );
 app.use("/api/v1/dashboard/", dashboardRouter );
-
+app.use("/api/v1/healthcheck/", healthcheckRouter );
 
 export { app };
