8fb98c008f07ec0b93820192b2f214529f350c6c
Subhajit Dash <dashsubhajit702@gmail.com>
2025-04-29 04:07:47 +0530

    feat: Organize comment routes for video and tweet with clear path structure

--- src/routes/comment.routes.js
+++ src/routes/comment.routes.js
@@ -1,23 +1,33 @@
 import { Router } from "express";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
 import {
+  addCommentOnTweet,
   addCommentOnVideo,
+  deleteTweetComment,
   deleteVideoComment,
+  getTweetComments,
   getVideoComments,
+  updateTweetComment,
   updateVideoComment,
 } from "../controllers/comment.controller.js";
 
 const router = Router();
 router.use(verifyJWT);
 
-router.route("/add-comment/:videoId").post(addCommentOnVideo);
+router.route("/video/add-comment/:videoId").post(addCommentOnVideo);
 
-router.route("/update-comment/:commentId").patch(updateVideoComment);
+router.route("/video/update-comment/:commentId").patch(updateVideoComment);
 
-router.route("/delete-comment/:commentId").delete(deleteVideoComment);
+router.route("/video/delete-comment/:commentId").delete(deleteVideoComment);
 
-//get all comments for given video id
-router.route("/all-comments/:videoId").get(getVideoComments);
+router.route("/video/all-comments/:videoId").get(getVideoComments);
 
+router.route("/tweet/add-comment/:tweetId").post(addCommentOnTweet);
 
-export default router
+router.route("/tweet/update-comment/:commentId").patch(updateTweetComment);
+
+router.route("/tweet/delete-comment/:commentId").delete(deleteTweetComment);
+
+router.route("/tweet/all-comments/:tweetId").get(getTweetComments);
+
+export default router;
